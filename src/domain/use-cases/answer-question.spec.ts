059fb4a85c8f14bd5134da599610b69b3cae82a1
Henrique <henriquim25@yahoo.com.br>
2024-08-09 16:23:03 -0300

    initial commit

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import { Answer } from "../entities/answer";
+
+const fakeAnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
+
+test("create an answer",async  () => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase(
+    fakeAnswersRepository
+  );
+
+  const answer =  await answerQuestionUseCase.execute({
+    content: "Resposta",
+    instructorId: "1",
+    questionId: "2",
+  });
+
+  expect(answer.content).toEqual("Resposta");
+});
