cfb19cdaf8e9659ae0b2887856dbd68dcce4f6f8
Henrique <henriquim25@yahoo.com.br>
2024-08-20 11:09:33 -0300

    feat: all use cases core implemented

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,45 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+import { DeleteQuestionCommentUseCase } from "./delete-question-comment";
+import { makeQuestionComment } from "test/factories/make-question-comment";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: DeleteQuestionCommentUseCase;
+
+describe("Delete Question Comment", () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentsRepository);
+  });
+  it("should be able to delete a  comment on question", async () => {
+    const questionComment = makeQuestionComment();
+
+    await inMemoryQuestionCommentsRepository.create(questionComment);
+
+    await sut.execute({
+      authorId: questionComment.authorId.toString(),
+      questionCommentId: questionComment.id.toString(),
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete a  comment on question", async () => {
+    const questionComment = makeQuestionComment({
+      authorId: new UniqueEntityID("author-01"),
+    });
+
+    await inMemoryQuestionCommentsRepository.create(questionComment);
+
+    expect(() => {
+      return sut.execute({
+        authorId: "author-02",
+        questionCommentId: questionComment.id.toString(),
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
